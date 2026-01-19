fdfdbb535bd274d8e33567966a678f1ca7127b44
farhandenn <farhandenn@gmail.com>
2026-01-19 09:09:50 +0700

    menambah konfirugasi

--- main.js
+++ main.js
@@ -10,3 +10,15 @@ import {
   doc,
   getDoc
 } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js"
+
+ // Firebase
+ const firebaseConfig = {
+  apiKey: "AIzaSyDf84tuPbEIrQ4b2jcU0MeXjg4OY3kE-yU",
+  authDomain: "insancemerlang-7c3fb.firebaseapp.com",
+  projectId: "insancemerlang-7c3fb",
+  storageBucket: "insancemerlang-7c3fb.firebasestorage.app",
+  messagingSenderId: "775357332019",
+  appId: "1:775357332019:web:25b794ac39eceb84f00146",
+  measurementId: "G-2T6Q5VM932"
+}
+
